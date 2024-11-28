501e78e2bdd042e80612e7ac48236e9e03a9072e
Mathias <mathias.dupont50@gmail.com>
2024-11-28 17:40:18 +0100

    refactor departments http controller

--- app/controllers/http/departments.controller.ts
+++ app/controllers/http/departments.controller.ts
@@ -1,20 +1,12 @@
 import { HttpContext } from '@adonisjs/core/http'
-import Department from '#models/department'
-import FuelPriceService from '#services/stations.service'
 import { inject } from '@adonisjs/fold'
-
+import DepartmentsServices from '#services/departments.services'
 @inject()
 export default class DepartmentsController {
-  constructor(private fuelPriceService: FuelPriceService) {}
+  constructor(private departmentsServices: DepartmentsServices) {}
   public async index({ view }: HttpContext) {
     try {
-      const departments = await Department.query().select('code', 'name').orderBy('code', 'asc')
-      const averagePrices = await this.fuelPriceService.getAveragePricesByDepartment()
-
-      const departmentsWithPrices = departments.map((dept) => ({
-        ...dept.toJSON(),
-        fuelPrices: averagePrices.filter((item) => item.dpt_id === dept.code),
-      }))
+      const departmentsWithPrices = await this.departmentsServices.getAveragePricesByDepartment()
 
       return view.render('pages/start', { departments: departmentsWithPrices })
     } catch (error) {
